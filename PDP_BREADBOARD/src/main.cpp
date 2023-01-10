dabb0316e14455b40ee8800b14bb8a927b223628
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-01-10 10:09:07 +0100

    Température

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 // Define the pins that we will use
 #define SENSOR 33
 #define LED 26
 
+DHT_Unified dht(SENSOR, DHTTYPE);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  sensor_t sensor;
 }
 
 void loop() {
   // Effectuer une mesure toutes les 5s
+  delay(5000);
 
   // Afficher l'humidité relative avec un peu de formatage
+sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
 
   // Afficher la température avec un peu de formatage
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 }
